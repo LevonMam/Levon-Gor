d4aceb776fd8bce316af64d917b3efa7a80dc32d
ArenGr <arengr.1990@gmail.com>
2019-02-19 18:19:04 +0400

    noric barev

--- Levon Mamajanyan/bot.cpp
+++ Levon Mamajanyan/bot.cpp
@@ -12,11 +12,11 @@ int  main () {
 
 string harcer (){
         int miavor = 0;
-        string anun;
+      
         cout<< "Barev inch e Dzer anuny? "<<endl;
         cin>>anun;
         cout << "Hargeli "<<anun<<"!" "Qani tarekan es"<<endl<<;
         cin>>anun;
-        if (anun < 18){
-        cout << "karox es masnakcel harcmany"<<endl;
+        if (anun < 25){
+        cout << "karox es masnakcel harcmany\n";
         cin>>anun;
