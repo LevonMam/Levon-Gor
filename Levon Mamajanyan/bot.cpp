ffe4ad9475ae18f52532a08e094b3b5470f2ad9c
ArenGr <arengr.1990@gmail.com>
2019-02-19 16:55:58 +0400

    barev

---
+++ Levon Mamajanyan/bot.cpp
@@ -0,0 +1,22 @@
+nclude <iostream>
+#include <string.h>
+using namespace std;
+
+string harcer();
+
+int  main () {
+
+        harcer ();
+        return 0 ;
+}
+
+string harcer (){
+        int miavor = 0;
+        string anun;
+        cout<< "Barev Dzez, inch e Dzer anuny? "<<endl;
+        cin>>anun;
+        cout << "Hargeli "<<anun<<"!" "Qani tarekan es"<<endl<<;
+        cin>>anun;
+        if (anun < 18){
+        cout << "karox es masnakcel harcmany"<<endl;
+        cin>>anun;
